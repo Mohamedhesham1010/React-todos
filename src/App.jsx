5b15df05c50e9b28f6058c3492b31522ab9228d6
Mohamedhesham1010 <mohamed.ahmedeltaher99@gmail.com>
2024-02-04 13:05:21 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import CssBaseline from "@mui/material/CssBaseline";
+import "./App.css";
+import TodoList from "./TodoList";
+
+
+function App() {
+  return (
+    <>
+    
+      <CssBaseline />
+     
+      <TodoList />
+    </>
+  );
+}
+
+export default App;
